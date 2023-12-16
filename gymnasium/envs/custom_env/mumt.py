ec1a34b0e65f8850f01c3885157934b50b77cc12
shanek16 <shane16@naver.com>
2023-12-16 19:17:31 +0900

    mumt render(action)->render() for SB3 compatibility

--- gymnasium/envs/custom_env/mumt.py
+++ gymnasium/envs/custom_env/mumt.py
@@ -144,9 +144,9 @@ class MUMT(Env):
         self.d = d  # target distance
         self.l = l  # coverage gap: coverage: d-l ~ d+l # noqa
         self.m = m  # of uavs
+        self.n = n  # of targets
         self.uavs = []
         self.uav_color = [(random.randrange(0, 11) / 10, random.randrange(0, 11) / 10, random.randrange(0, 11) / 10) for _ in range(m)]
-        self.n = n  # of targets
         self.targets = []
         self.r_c = r_c  # charge station radius
         self.step_count = None
@@ -154,11 +154,11 @@ class MUMT(Env):
         self.num2str = {0: "charge", 1: "target_1"}
         self.max_step = max_step
         self.viewer = None
-        self.SAVE_FRAMES_PATH = "../../../../visualized/2U2T" # example. save frames path is set at surveillance_PPO.py
+        self.SAVE_FRAMES_PATH = f"../../../../visualized/{self.m}U{self.n}T"
         self.episode_counter = 0
         self.frame_counter = 0
         self.save_frames = False
-        self.q_init()
+        self.action = None
 
         # initialization for Dynamic Programming
         self.n_r = 800
@@ -169,7 +169,6 @@ class MUMT(Env):
         self.distance_keeping_result00 = np.load(current_file_path+ os.path.sep + "v1_80_2a_dkc_val_iter.npz")
         self.distance_keeping_straightened_policy00 = self.distance_keeping_result00["policy"] # .data
         self.time_optimal_straightened_policy00 = np.load(current_file_path+ os.path.sep + "v1_terminal_40+40_2a_toc_policy_fp64.npy")
-        # print('shape of time_optimal_straightened_policy00',np.shape(self.time_optimal_straightened_policy00)) # 288000
 
         self.states = States(
             np.linspace(0.0, 80.0, self.n_r, dtype=np.float32),
@@ -234,47 +233,11 @@ class MUMT(Env):
             target_states = np.array([target1_r * np.cos(target1_beta), target1_r * np.sin(target1_beta)]).T
             ages = [0] * self.n
         else:
-            target_states = target_pose  # Assuming target_pose is an iterable of target states
+            target_states, ages = target_pose  # Assuming target_pose is an iterable of target states
         # Create Target instances
         for i in range(self.n):
             self.targets.append(self.Target(state=target_states[i], age=ages[i]))
         return self.dict_observation, {}
-
-    def q_init(self):
-        self.n_alpha = 10
-        # simple version
-        # self.target_discretized_r_space = np.arange(0,81,20)
-        # self.charge_discretized_r_space = np.arange(0,51,10)
-        # self.target_alpha_space = np.linspace(-np.pi, np.pi - np.pi / self.n_alpha, self.n_alpha, dtype=np.float32)
-        # self.charge_alpha_space = np.linspace(-np.pi, np.pi - np.pi / self.n_alpha, self.n_alpha, dtype=np.float32)
-        # self.battery_space = np.arange(0, 3100,1000)
-
-        # complex version
-        self.target_discretized_r_space = np.array([0,4,6,8,10,12,14,16,20,30,40,60,80])
-        self.charge_discretized_r_space = np.array([0,2,3,5,6,10,20,30,40])
-        self.target_alpha_space = np.linspace(-np.pi, np.pi - np.pi / self.n_alpha, self.n_alpha, dtype=np.float32)
-        self.charge_alpha_space = np.linspace(-np.pi, np.pi - np.pi / self.n_alpha, self.n_alpha, dtype=np.float32)
-        self.battery_space = np.concatenate([np.arange(0, 500, 100), np.arange(500, 3100, 500)])
-
-        self.age_space = np.arange(0, 1001, 100) #changeage
-        self.UAV1Target1_result00 = np.load(current_file_path + f"/1U1T_s6_age1000:100_gamma_{self.discount}_dt_{self.dt}_{'val'}_iter.npz")
-        self.UAV1Target1_straightened_policy00 = self.UAV1Target1_result00["policy"]
-        self.UAV1Target1_values00 = self.UAV1Target1_result00["values"]
-        # print('shape of UAV1Target1_straightened_policy00: ', np.shape(self.UAV1Target1_straightened_policy00))
-        # print('shape of UAV1Target1_values00: ', np.shape(self.UAV1Target1_values00))
-
-        self.uav1target1_states = States(
-            # uav1_target1
-            self.target_discretized_r_space, # [0]
-            self.target_alpha_space,         # [1]
-            # uav1_charge
-            self.charge_discretized_r_space, # [2]
-            self.charge_alpha_space,         # [3]
-            # battery_state
-            self.battery_space,              # [4]
-            self.age_space,                  # [5]
-            cycles=[np.inf, np.pi*2, np.inf, np.pi*2, np.inf, np.inf]
-        )
 
     def toc_get_action(self, state):
         S, P = self.states.computeBarycentric(state)
@@ -318,11 +281,11 @@ class MUMT(Env):
                 return 0
 
     def step(self, action):
+        self.action = action
         terminal = False
         truncated = False
         action = np.squeeze(action)
         reward = 0
-        # TODO: if action is a 0-d array, convert to 1d array
         if action.ndim == 0:
             action = np.expand_dims(action, axis=0)
         for uav_idx, uav_action in enumerate(action):
@@ -339,28 +302,15 @@ class MUMT(Env):
             reward += -self.targets[target_idx].age
         reward = reward / self.n # average reward of all targets
         if self.save_frames and int(self.step_count) % 6 == 0:
-            image = self.render(action, mode="rgb_array")
+            image = self.render(mode="rgb_array")
             path = os.path.join(
                 self.SAVE_FRAMES_PATH,
                 f"{self.episode_counter:03d}",
                 f"{self.frame_counter+1:04d}.bmp",
             )
             image = Image.fromarray(image)
-            draw = ImageDraw.Draw(image)
-            # left upper corner
-            text = [
-                f"r_c: {self.uav1.obs[0]}, a_c: {self.uav1.obs[1]}",
-                f"r_t: {self.target1.obs[0]}, a_t: {self.target1.obs[1]}",
-                f"max_Q: {self.max_Q:.2f}",
-                f"dQ: {self.max_Q - self.min_Q:.2f}",
-                f"argmax(Q0,Q1): {self.argmax_Q}",
-                "battery: {}".format(self.uav1.battery),
-                "age: {}".format(self.target1.age),
-                "Reward: {}".format(reward)
-            ]
-            for i, t in enumerate(text):
-                draw.text((0, 20*i), t, color=(255, 255, 255), font=self.font)
-                image.save(path)
+            '''setup text label here'''
+            image.save(path)
             self.frame_counter += 1
         self.step_count += 1
         if self.step_count >= self.max_step:
@@ -439,7 +389,7 @@ class MUMT(Env):
             reward += -target1_copy.age*discount**i
         return dry_dict_observation, reward, terminal, truncated, {}
 
-    def render(self, action, mode="human"):
+    def render(self, mode="human"):
         if self.viewer is None:
             self.viewer = rendering.Viewer(1000, 1000)
             bound = int(40 * 1.05)
@@ -466,9 +416,9 @@ class MUMT(Env):
             target_circle = self.viewer.draw_circle(
                 radius=1, x=target_x, y=target_y, filled=True
             )
-            if target_idx + 1 in action:
+            if target_idx + 1 in self.action:
                 try:
-                    target_circle.set_color(*self.uav_color[int(np.where(action == target_idx + 1)[0])])  # yellow
+                    target_circle.set_color(*self.uav_color[int(np.where(self.action == target_idx + 1)[0])])  # yellow
                 except:
                     target_circle.set_color(1, 1, 0)  # multiple uavs are after one target
             else:
@@ -477,9 +427,9 @@ class MUMT(Env):
         # charge station @ origin
         charge_station = self.viewer.draw_circle(radius=self.r_c, filled=True)
         
-        if 0 in action:
+        if 0 in self.action:
             try:
-                charge_station.set_color(*self.uav_color[int(np.where(action == 0)[0])])  # yellow
+                charge_station.set_color(*self.uav_color[int(np.where(self.action == 0)[0])])  # yellow
             except:
                 charge_station.set_color(1, 1, 0)  # multiple uavs are after the charge station
             for uav_idx, uav in enumerate(self.uavs):
@@ -536,45 +486,11 @@ class MUMT(Env):
         dictionary_obs["age"] = np.float32([self.targets[target_id].age for target_id in range(self.n)])
 
         return dictionary_obs
-    
-    def compare_q_value(self):
-        # dry_step with 1 env
-        # 1) get reward for each action: 0 & 1
-        state0, reward0, _, _, _ = self.dry_step(action=0)
-        state1, reward1, _, _, _ = self.dry_step(action=1)
-
-        # 2) get Value for each action
-        S0, P0 = self.uav1target1_states.computeBarycentric(state0)
-        value0 = 0
-        for s0, p0 in zip(S0, P0):
-            value0 += p0 * self.UAV1Target1_values00[s0]
-
-        S1, P1 = self.uav1target1_states.computeBarycentric(state1)
-        value1 = 0
-        for s1, p1 in zip(S1, P1):
-            value1 += p1 * self.UAV1Target1_values00[s1]
-
-        self.Q0 = reward0+self.discount*value0
-        self.Q1 = reward1+self.discount*value1
-        self.max_Q = max(self.Q0, self.Q1)
-        self.min_Q = min(self.Q0, self.Q1)
-        self.argmax_Q = np.argmax([self.Q0, self.Q1])
-        # print(f'max_Q: {max_Q:.0f}', end=' | ')
-        # print(f'argmax_Q: {self.argmax_Q}', end=' | ')
-        # print(f'maxQ - Q: {self.max_Q - self.min_Q:.0f}', end=' | ')
-        # print(f'Q0: {self.Q0:.2f}, Q1: {self.Q1:.2f}')
 
 
 if __name__ == "__main__":
-    # test return of dry_step == step
-    ''' env = UAV1Target1()
-    env.reset()
-    state0, reward0, _, _, _ = env.dry_step(action=0)
-    state, reward, _, _, _ = env.step(action=0)
-    print(state0)
-    print(state)'''
     m=2
-    n=1
+    n=2
     uav_env = MUMT(m=m, n=n)
 
     # Number of features
@@ -584,9 +500,15 @@ if __name__ == "__main__":
     print("action_sample: ", action_sample)
     print('uav_env.observation_space:', uav_env.observation_space)
     print('uav_env.action_space.n: ', uav_env.action_space)
-        
+
     # testing env: alternating action
-    '''obs, _ = uav_env.reset()
+    # target1_r = np.random.uniform(20, 35, self.n)  # 0~ D-d
+    # target1_beta = np.random.uniform(-np.pi, np.pi, self.n)
+    target_states = np.array([np.array([0, 24]), np.array([30, 20])]).T
+    ages = [100] * n
+    batteries = np.array([3000, 1000])
+
+    obs, _ = uav_env.reset(target_pose=(target_states, ages), batteries=batteries)
     step = 0
     while step < 5000:
         step += 1
@@ -595,7 +517,7 @@ if __name__ == "__main__":
         obs, reward, _, truncated, _ = uav_env.step(action_sample)
         bat = obs['battery']
         print(f'step: {step} | battery: {bat} | reward: {reward}')
-        uav_env.render(action_sample)'''
+        uav_env.render()
     
     # testing env: heuristic policy
     '''repitition = 10
@@ -635,7 +557,7 @@ if __name__ == "__main__":
             # print(f'step: {step} | battery: {bat} | reward: {reward}') #, end=' |')
             # print(f'action: {action}')#, end=' |')
             # uav_env.print_q_value()
-            # uav_env.render(action, mode='rgb_array')
+            # uav_env.render(mode='rgb_array')
         print(f'{i}: {total_reward}')   
         avg_reward += total_reward
     avg_reward /= repitition
